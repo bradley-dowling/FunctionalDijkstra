0a6ba1d3c5e58131f055377f78952af3ceb1c1b1
Bradley Dowling <bradley.dowling@wsu.edu>
2022-04-24 23:45:09 -0700

    created graph

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,16 @@
 object Main extends App {
 
+  // create the graph
+  val graph = Map(
+    "home" -> Seq(("a", 1), ("c", 0), ("d", 1)),
+    "a" -> Seq(("b", 1), ("e", 2)),
+    "b" -> Seq(("c", 2), ("e", 7)),
+    "c" -> Seq(("e", 6)),
+    "d" -> Seq(("f", 5), ("g", 8)),
+    "e" -> Seq(("h", 3), ("f", 4)),
+    "f" -> Seq(("g", 0), ("i", 3)),
+    "g" -> Seq(("i", 3)),
+    "h" -> Seq(("office", 8)),
+    "i" -> Seq(("office", 1))
+  )
 }
